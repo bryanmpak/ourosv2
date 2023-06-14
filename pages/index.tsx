55a21a333130fdc57db70a200a3f6cd60481f156
bryanmpak <bryan.m.pak@gmail.com>
2023-06-14 11:36:13 +0900

    minor change

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import ImageGallery from "../components/ImageGallery"
 export default function Home() {
   return (
     <>
+      {/* later: refactor the whole thing to be a bit more productive */}
       <Countdown />
       <DailyQuote />
       <ImageGallery />
